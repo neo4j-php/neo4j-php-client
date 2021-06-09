5c3701a207bbd6cfc143fa188e4630d9c300acc0
ghlen <ghlen@pm.me>
2021-06-09 15:43:24 +0200

    fixed typing OGM Formatter

--- src/Formatter/OGMFormatter.php
+++ src/Formatter/OGMFormatter.php
@@ -34,7 +34,10 @@ use Psr\Http\Message\ResponseInterface;
  * @psalm-type OGMTypes = string|\Laudis\Neo4j\Types\Date|\Laudis\Neo4j\Types\DateTime|\Laudis\Neo4j\Types\Duration|\Laudis\Neo4j\Types\LocalDateTime|\Laudis\Neo4j\Types\LocalTime|\Laudis\Neo4j\Types\Time|int|float|bool|null|\Laudis\Neo4j\Types\CypherList|\Laudis\Neo4j\Types\CypherMap|\Laudis\Neo4j\Types\Node|\Laudis\Neo4j\Types\Relationship|\Laudis\Neo4j\Types\Path|\Laudis\Neo4j\Types\Cartesian3DPoint|\Laudis\Neo4j\Types\CartesianPoint|\Laudis\Neo4j\Types\WGS84Point|\Laudis\Neo4j\Types\WGS843DPoint
  * @implements FormatterInterface<CypherList<CypherMap<OGMTypes>>>
  *
- * @psalm-type CypherResult = array{columns: list<string>, data: list<array{row: list<scalar|array|null>, meta: array, graph: array{nodes: array, relationships: array}}>}
+ * @psalm-import-type NodeArray from \Laudis\Neo4j\Formatter\Specialised\HttpOGMArrayTranslator
+ * @psalm-import-type RelationshipArray from \Laudis\Neo4j\Formatter\Specialised\HttpOGMArrayTranslator
+ *
+ * @psalm-type CypherResult = array{columns: list<string>, data: list<array{row: list<scalar|array|null>, meta: array, graph: array{nodes: list<NodeArray>, relationships: list<RelationshipArray>}}>}
  *
  * @psalm-import-type BoltMeta from \Laudis\Neo4j\Contracts\FormatterInterface
  */
