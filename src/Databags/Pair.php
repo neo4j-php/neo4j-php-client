d3616104533d24947797cef02cc740d6c47dc189
ghlen <ghlen@pm.me>
2021-09-05 23:11:38 +0200

    reworked CypherMap to work with array under the hood

--- src/Databags/Pair.php
+++ src/Databags/Pair.php
@@ -1,8 +1,54 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the Laudis Neo4j package.
+ *
+ * (c) Laudis technologies <http://laudis.tech>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Laudis\Neo4j\Databags;
 
-class Pair
+/**
+ * @template TKey
+ * @template TValue
+ *
+ * @psalm-immutable
+ */
+final class Pair
 {
+    /** @var TKey */
+    private $key;
+    /** @var TValue */
+    private $value;
 
+    /**
+     * @param TKey   $key
+     * @param TValue $value
+     */
+    public function __construct($key, $value)
+    {
+        $this->key = $key;
+        $this->value = $value;
+    }
+
+    /**
+     * @return TKey
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    /**
+     * @return TValue
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
 }
