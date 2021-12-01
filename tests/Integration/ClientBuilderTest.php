79985559a35475df7abc6d779144d20a33f74805
ghlen <ghlen@pm.me>
2021-12-01 15:48:49 +0100

    split tests in integration and unit

--- tests/Integration/ClientBuilderTest.php
+++ tests/Integration/ClientBuilderTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Laudis\Neo4j\Tests\Unit;
+namespace Laudis\Neo4j\Tests\Integration;
 
 use function explode;
 use function getenv;
