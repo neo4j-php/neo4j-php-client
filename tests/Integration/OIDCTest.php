942555cc761d30422ecd6da53ee6d4dbba61b4e2
ghlen <ghlen@pm.me>
2021-12-30 17:25:42 +0100

    ran php cs fixer

--- tests/Integration/OIDCTest.php
+++ tests/Integration/OIDCTest.php
@@ -12,11 +12,11 @@
 namespace Laudis\Neo4j\Tests\Integration;
 
 use function array_key_exists;
+use function is_string;
 use Laudis\Neo4j\Authentication\Authenticate;
 use Laudis\Neo4j\ClientBuilder;
 use Laudis\Neo4j\Http\HttpDriver;
 use Monolog\Test\TestCase;
-use function is_string;
 
 /**
  * @psalm-suppress MissingConstructor
