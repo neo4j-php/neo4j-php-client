17b1840868ea906d057a8efb06e95ef6b08a5bc4
ghlen <ghlen@pm.me>
2022-07-10 22:35:25 +0200

    extended correct testcase

--- tests/Integration/OIDCTest.php
+++ tests/Integration/OIDCTest.php
@@ -16,7 +16,7 @@ use function is_string;
 use Laudis\Neo4j\Authentication\Authenticate;
 use Laudis\Neo4j\ClientBuilder;
 use Laudis\Neo4j\Http\HttpDriver;
-use Monolog\Test\TestCase;
+use PHPUnit\Framework\TestCase;
 
 /**
  * @psalm-suppress MissingConstructor
