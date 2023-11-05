5c8ce16ab1f5cfdbf37a81ba95d1de5ac5ff9f46
Ghlen Nagels <ghlen@pm.me>
2023-11-05 17:07:34 +0530

    added Reset message

---
+++ src/Bolt/Messages/GoodBye.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Laudis\Neo4j\Bolt\Messages;
+
+use Bolt\protocol\V4_4;
+use Bolt\protocol\V5;
+use Bolt\protocol\V5_1;
+use Bolt\protocol\V5_2;
+use Bolt\protocol\V5_3;
+use Laudis\Neo4j\Contracts\MessageInterface;
+
+/**
+ * @internal
+ *
+ * @see https://neo4j.com/docs/bolt/current/bolt/message/#messages-goodbye
+ */
+class GoodBye implements MessageInterface
+{
+    public function send(V4_4|V5|V5_2|V5_1|V5_3 $bolt): void
+    {
+        $bolt->goodbye();
+    }
+}
