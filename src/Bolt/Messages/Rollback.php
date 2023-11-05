755effe0e74653db83d5441df87b0055613c39c0
Ghlen Nagels <ghlen@pm.me>
2023-11-05 17:32:00 +0530

    created commit and rollback message

---
+++ src/Bolt/Messages/Rollback.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Laudis\Neo4j\Bolt\Messages;
+
+use Bolt\protocol\V4_4;
+use Bolt\protocol\V5;
+use Bolt\protocol\V5_1;
+use Bolt\protocol\V5_2;
+use Bolt\protocol\V5_3;
+use Laudis\Neo4j\Contracts\MessageInterface;
+
+class Rollback implements MessageInterface
+{
+    public function send(V4_4|V5|V5_2|V5_1|V5_3 $bolt): void
+    {
+        $bolt->rollback();
+    }
+}
