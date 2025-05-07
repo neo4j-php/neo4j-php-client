3704d082a06e244777554dfdc70d294d33cd2ce2
Nabeel Parkar <nabeelparkar99@gmail.com>
2025-05-07 18:22:00 +0530

    ci: Add testkit (#258)
    * ci: Add testkit
    
    * Test commit to trigger CI
    
    * ci: Rename workflow
    
    * chore: Only export env vars if they don't already exist
    
    * chore: Restart testkit_backend after composer install
    
    * ci: Add --rm to docker compose run commands
    
    * ci: Remove redundant pull request branch specification from workflow files
    
    * fix: Rename protocol to protocolVersion in ServerInfo::toArray
    
    * fix: Update test execution logic in testkit.sh to capture exit codes

--- testkit-backend/testkit.sh
+++ testkit-backend/testkit.sh
@@ -2,14 +2,12 @@
 
 TESTKIT_VERSION=5.0
 
-export TEST_NEO4J_HOST=neo4j
-export TEST_NEO4J_USER=neo4j
-export TEST_NEO4J_PASS=testtest
-export TEST_DRIVER_NAME=php
+[ -z "$TEST_NEO4J_HOST" ] && export TEST_NEO4J_HOST=neo4j
+[ -z "$TEST_NEO4J_USER" ] && export TEST_NEO4J_USER=neo4j
+[ -z "$TEST_NEO4J_PASS" ] && export TEST_NEO4J_PASS=testtest
+[ -z "$TEST_DRIVER_NAME" ] && export TEST_DRIVER_NAME=php
 
-
-TEST_DRIVER_REPO=$(realpath ..)
-export TEST_DRIVER_REPO
+[ -z "$TEST_DRIVER_REPO" ] && TEST_DRIVER_REPO=$(realpath ..) && export TEST_DRIVER_REPO
 
 if [ "$1" == "--clean" ]; then
     if [ -d testkit ]; then
@@ -18,7 +16,7 @@ if [ "$1" == "--clean" ]; then
 fi
 
 if [ ! -d testkit ]; then
-    git clone git@github.com:neo4j-drivers/testkit.git
+    git clone https://github.com/neo4j-drivers/testkit.git
     if [ "$(cd testkit && git branch --show-current)" != "${TESTKIT_VERSION}" ]; then
         (cd testkit && git checkout ${TESTKIT_VERSION})
     fi
@@ -31,12 +29,20 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
+# python3 main.py --tests UNIT_TESTS
+
 echo "Starting tests..."
-# exec python3 main.py --tests UNIT_TESTS
-(exec python3 -m unittest tests.neo4j.test_authentication.TestAuthenticationBasic) || true
-#echo "TestAuthenticationBasic Done"
-python3 -m unittest tests.neo4j.test_bookmarks.TestBookmarks || true
-echo "TestBookmarks Done"
-#python3 -m unittest tests.neo4j.test_session_run.TestSessionRun.test_recover_from_fail_on_streaming|| true
-#echo "TestSessionRun Done"
+
+EXIT_CODE=0
+
+python3 -m unittest tests.neo4j.test_authentication.TestAuthenticationBasic || EXIT_CODE=1
+
+python3 -m unittest tests.neo4j.test_bookmarks.TestBookmarks || EXIT_CODE=1
+
+python3 -m unittest tests.neo4j.test_session_run.TestSessionRun.test_iteration_nested || EXIT_CODE=1
+
+# Uncomment to run this test as well
+# python3 -m unittest tests.neo4j.test_session_run.TestSessionRun.test_recover_from_fail_on_streaming || EXIT_CODE=1
+
+exit $EXIT_CODE
 
