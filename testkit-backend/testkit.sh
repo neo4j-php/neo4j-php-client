da7186c8133e9a4524e5d4934901a4f14bcd3f7e
p123-stack <pratiksha@nagels.tech>
2025-05-12 13:40:19 +0530

    fixed the testkit tests for class TestDirectDriver

--- testkit-backend/testkit.sh
+++ testkit-backend/testkit.sh
@@ -39,7 +39,7 @@ python3 -m unittest tests.neo4j.test_authentication.TestAuthenticationBasic || E
 python3 -m unittest tests.neo4j.test_bookmarks.TestBookmarks || EXIT_CODE=1
 
 # This test is still failing so we skip it
-# python3 -m unittest tests.neo4j.test_session_run.TestSessionRun.test_autocommit_transactions_should_support_timeouttest_autocommit_transactions_should_support_timeout|| EXIT_CODE=1
+ python3 -m unittest tests.neo4j.test_session_run.TestSessionRun.test_autocommit_transactions_should_support_timeouttest_autocommit_transactions_should_support_timeout|| EXIT_CODE=1
 python3 -m unittest tests.neo4j.test_session_run.TestSessionRun.test_iteration_smaller_than_fetch_size
 python3 -m unittest tests.neo4j.test_session_run.TestSessionRun.test_can_return_node
 python3 -m unittest tests.neo4j.test_session_run.TestSessionRun.test_can_return_relationship
@@ -59,8 +59,11 @@ python3 -m unittest tests.neo4j.test_session_run.TestSessionRun.test_fails_on_ba
 python3 -m unittest tests.neo4j.test_session_run.TestSessionRun.test_fails_on_missing_parameter
 python3 -m unittest tests.neo4j.test_session_run.TestSessionRun.test_long_string
 
-#python3 -m unittest tests.neo4j.test_direct_driver.TestDirectDriver|| EXIT_CODE=1
-#python3 -m unittest tests.neo4j.test_summary.TestSummary|| EXIT_CODE=1
-
+# This test is still failing so we skip it
+python3 -m unittest tests.neo4j.test_direct_driver.TestDirectDriver.test_custom_resolver|| EXIT_CODE=1
+python3 -m unittest tests.neo4j.test_direct_driver.TestDirectDriver.test_fail_nicely_when_using_http_port|| EXIT_CODE=1
+python3 -m unittest tests.neo4j.test_direct_driver.TestDirectDriver.test_supports_multi_db|| EXIT_CODE=1
+python3 -m unittest tests.neo4j.test_direct_driver.TestDirectDriver.test_multi_db|| EXIT_CODE=1
+python3 -m unittest tests.neo4j.test_direct_driver.TestDirectDriver.test_multi_db_various_databases|| EXIT_CODE=1
 exit $EXIT_CODE
 
