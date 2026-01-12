b7649ecf42fa544d6b50f0471748a25e7777de9d
Pratiksha Zalte <pratiksha@nagels.tech>
2026-01-12 20:09:11 +0530

    fixed static analysis

--- testkit-backend/src/Handlers/TransactionClose.php
+++ testkit-backend/src/Handlers/TransactionClose.php
@@ -48,4 +48,3 @@ final class TransactionClose implements RequestHandlerInterface
         return new TransactionResponse($request->getTxId());
     }
 }
-
--- testkit-backend/src/Requests/TransactionCloseRequest.php
+++ testkit-backend/src/Requests/TransactionCloseRequest.php
@@ -29,4 +29,3 @@ final class TransactionCloseRequest
         return $this->txId;
     }
 }
-
