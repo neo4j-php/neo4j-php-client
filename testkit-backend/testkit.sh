3d0fb6e1d1591b44bdc2d900c33584e7c1362c27
Pratiksha Zalte <pratiksha@nagels.tech>
2026-01-12 19:14:30 +0530

    replace TimeoutException with Neo4jException for recv timeout errors

--- testkit-backend/testkit.sh
+++ testkit-backend/testkit.sh
@@ -11,23 +11,11 @@ TESTKIT_VERSION=5.0
 
 [ -z "$TEST_DRIVER_REPO" ] && TEST_DRIVER_REPO=$(realpath ..) && export TEST_DRIVER_REPO
 
-if [ "$1" == "--clean" ]; then
-    if [ -d testkit ]; then
-        rm -rf testkit
-    fi
-fi
-
-if [ ! -d testkit ]; then
-    git clone https://github.com/neo4j-drivers/testkit.git
-    if [ "$(cd testkit && git branch --show-current)" != "${TESTKIT_VERSION}" ]; then
-        (cd testkit && git checkout ${TESTKIT_VERSION})
-    fi
-fi
 #else
 #    (cd testkit && git pull)
 #fi
 
-cd testkit || (echo 'cannot cd into testkit' && exit 1)
+cd ../testkit || (echo 'cannot cd into testkit' && exit 1)
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt > /dev/null 2>&1
